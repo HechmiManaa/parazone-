715ccdfaa6a057bc828a9f40ee1345e7a13d9665
rayenbenhassen07 <rayenbenhassen07@gmail.com>
2024-06-19 17:38:24 +0100

    dfsdfaasd

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -12,18 +12,18 @@ export function ProductCard({
   store_id,
 }: Product) {
   return (
-    <div className="bg-white rounded-lg shadow-lg overflow-hidden w-80 cursor-pointer h-full hover:bg-gray-50">
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden h-56 lg:h-80  cursor-pointer hover:bg-gray-50 ">
       <div className="flex items-center justify-center mb-4">
         <Image
           alt={title}
           src={`${product_img}`}
           width={500}
           height={500}
-          className=" object-cover rounded-full w-full h-60"
+          className=" object-cover rounded-full w-full h-20 lg:h-44"
         />
       </div>
       <div className="p-4 h-22 flex flex-col justify-around">
-        <p className="text-sm font-bold text-gray-900">{title}</p>
+        <p className="text-xs lg:text-sm font-bold text-gray-900">{title}</p>
         <div>
           <p className="text-xs font-semibold text-gray-600 py-2">
             Marque: {brand_id?.title}
